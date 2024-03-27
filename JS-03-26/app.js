df3ea87e62a2e840ee7754a8e841004bf0294259
aivaras23 <aivaraspal1@gmail.com>
2024-03-27 08:48:47 +0200

    added validation

--- JS-03-26/app.js
+++ JS-03-26/app.js
@@ -9,8 +9,13 @@ const result = document.getElementById('result');
 const calculation = () => {
     const getSvoris = svoris.valueAsNumber;
     const getUgis = ugis.valueAsNumber;
-    const KMI = (getSvoris / (getUgis ** 2)) * 10000;
-    result.innerHTML = `${KMI.toFixed(1)}`;
+    if (isNaN(getSvoris) || isNaN(getUgis)) {
+        result.innerHTML = `Įvesti skaičiai yra neteisingi`
+    }
+    else {
+        const KMI = (getSvoris / (getUgis ** 2)) * 10000;
+        result.innerHTML = `${KMI.toFixed(1)}`;
+    }
 }
 
 calcBtn.onclick = calculation;
