7b634f2d7129449bb5c40b9fdbb5d01df1d01515
aivaras23 <aivaraspal1@gmail.com>
2024-04-09 21:00:10 +0300

    update

--- JS-04-09/app.js
+++ JS-04-09/app.js
@@ -43,16 +43,36 @@ class Triangle {
         const C = this.edgeC;
         const semiP = (A + B + C) / 2;
         const plotas = Math.sqrt(semiP * (semiP - A) * (semiP - B) * (semiP - C));
-        return `Trikampio plotas: ${plotas.toFixed(2)}`;
+        return plotas;
+    }
+    largerTriangle(t) {
+        const one = this.getArea();
+        const two = t.getArea();
+        if (one >= two) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
-let newTriangle = new Triangle(5, 7, 8);
-let newTriangle2 = new Triangle(7, 9, 12);
+let newTriangle = new Triangle(22, 19, 25);
+let newTriangle2 = new Triangle(16, 12, 19);
+console.log(newTriangle2.checkTriangle());
 console.log(newTriangle.toString());
 console.log(newTriangle.getPerimeter());
-console.log(newTriangle.getArea());
-/*
-   public largerTriangle(t:Triangle) {
-  
-    }
-    */ 
+console.log(`Trikampio plotis: ${newTriangle.getArea()}`);
+console.log(newTriangle2.toString());
+console.log(newTriangle2.getPerimeter());
+console.log(`Trikampio plotis: ${newTriangle2.getArea()}`);
+console.log(`Ar pirmas trimapis didesnis: ${newTriangle.largerTriangle(newTriangle2)}`);
+const trianglesArr = [];
+trianglesArr.push(new Triangle(13, 12, 14));
+trianglesArr.push(new Triangle(9, 10, 7));
+trianglesArr.push(new Triangle(10, 10, 13));
+let totalArea = 0;
+for (const triangle of trianglesArr) {
+    const area = triangle.getArea();
+    totalArea += area;
+}
+console.log(`Visu trikampiu plotu suma: ${totalArea.toFixed(2)}`);
--- JS-04-09/app.ts
+++ JS-04-09/app.ts
@@ -41,31 +41,55 @@ class Triangle {
         return `Trikampio perimetras: ${this.edgeA + this.edgeB + this.edgeC}`
     }
 
-    public getArea() {
+    public getArea():number {
         const A = this.edgeA;
         const B = this.edgeB;
         const C = this.edgeC;
         const semiP = (A + B + C) / 2;
         const plotas = Math.sqrt(semiP * (semiP - A) * (semiP - B) * (semiP - C));
-        return `Trikampio plotas: ${plotas.toFixed(2)}`
+        return plotas;
     }
 
+    public largerTriangle(t:Triangle) {
+        const one = this.getArea();
+        const two = t.getArea();
+
+        if(one >= two) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
  
 }
 
-let newTriangle = new Triangle(5,7,8);
-let newTriangle2 = new Triangle(7,9,12);
+let newTriangle = new Triangle(12,9,15);
+let newTriangle2 = new Triangle(16,12,19);
+
+console.log(newTriangle2.checkTriangle());
 
 console.log(newTriangle.toString());
 console.log(newTriangle.getPerimeter());
-console.log(newTriangle.getArea());
+console.log(`Trikampio plotis: ${newTriangle.getArea()}`);
+
+console.log(newTriangle2.toString());
+console.log(newTriangle2.getPerimeter());
+console.log(`Trikampio plotis: ${newTriangle2.getArea()}`);
+console.log(`Ar pirmas trimapis didesnis: ${newTriangle.largerTriangle(newTriangle2)}`);
 
 
+const trianglesArr:Triangle[] = [];
+
+trianglesArr.push(new Triangle(13,12,14));
+trianglesArr.push(new Triangle(9,10,7));
+trianglesArr.push(new Triangle(10,10,13));
 
 
+let totalArea = 0;
+for(const triangle of trianglesArr) {
+    const area = triangle.getArea();
+    totalArea += area;
+}
+ console.log(`Visu trikampiu plotu suma: ${totalArea.toFixed(2)}`)
 
-/*
-   public largerTriangle(t:Triangle) {
-  
-    }
-    */
