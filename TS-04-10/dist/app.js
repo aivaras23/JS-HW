72c47fd8dd0f1d5f1ba80940d2d6bb1093b61f57
aivaras23 <aivaraspal1@gmail.com>
2024-04-10 22:14:36 +0300

    update3

--- TS-04-10/dist/app.js
+++ TS-04-10/dist/app.js
@@ -1,4 +1,9 @@
 "use strict";
+const vardas = document.getElementById('vardas');
+const pavarde = document.getElementById('pavarde');
+const atlyginimas = document.getElementById('atlyginimas');
+const employeeList = document.getElementById('employeeList');
+const addBtn = document.getElementById('addBtn');
 class Darbuotojas {
     constructor(vardas, pavarde, atlyginimas) {
         this.vardas = vardas;
@@ -24,21 +29,44 @@ class Darbuotojas {
         return this.atlyginimas;
     }
     gpm() {
-        return `gyventojo pajamų mokestis(GPM): ${(this.atlyginimas * 0.20).toFixed(2)}`;
+        return (this.atlyginimas * 0.20).toFixed(2);
     }
     psd() {
-        return `privalomusis sveikatos draudimas(PSD): ${(this.atlyginimas * 0.0698).toFixed(2)}`;
+        return (this.atlyginimas * 0.0698).toFixed(2);
     }
     vsd() {
-        return `valstybinis socialinis draudimas(VSD): ${(this.atlyginimas * 0.1252).toFixed(2)}`;
+        return (this.atlyginimas * 0.1252).toFixed(2);
     }
 }
 let newDarbuotojas = new Darbuotojas('Jonas', 'Jonaitis', 1600);
-console.log(newDarbuotojas.gpm());
-console.log(newDarbuotojas.psd());
-console.log(newDarbuotojas.vsd());
+// console.log(newDarbuotojas.gpm());
+// console.log(newDarbuotojas.psd());
+// console.log(newDarbuotojas.vsd());
 const darbuotojuArr = [];
-darbuotojuArr.push(new Darbuotojas('Ona', 'Onaite', 1600));
-darbuotojuArr.push(new Darbuotojas('Antantas', 'Antanaitis', 2000));
-darbuotojuArr.push(new Darbuotojas('Petras', 'Petraitis', 1400));
-console.log(darbuotojuArr);
+// darbuotojuArr.push(new Darbuotojas('Ona','Onaite',1600));
+// darbuotojuArr.push(new Darbuotojas('Antantas','Antanaitis',2000));
+// darbuotojuArr.push(new Darbuotojas('Petras','Petraitis',1400));
+// HTML DOM
+const employees = () => {
+    const getName = vardas.value;
+    const getPavarde = pavarde.value;
+    const getAtlyginimas = atlyginimas.valueAsNumber;
+    const naujasDarbuotojas = new Darbuotojas(getName, getPavarde, getAtlyginimas);
+    darbuotojuArr.push(naujasDarbuotojas);
+    employeeList.innerHTML = '';
+    darbuotojuArr.forEach((darbuotojas, index) => {
+        const li = document.createElement('li');
+        li.innerHTML = `${darbuotojas._vardas} ${darbuotojas._pavarde} ${darbuotojas._atlyginimas}€ <br> GPM:${darbuotojas.gpm()} PSD:${darbuotojas.psd()} VSD:${darbuotojas.vsd()}`;
+        employeeList.appendChild(li);
+        const dltBtn = document.createElement('button');
+        dltBtn.textContent = 'Ištrinti';
+        dltBtn.classList.add('delete-button');
+        dltBtn.onclick = () => {
+            darbuotojuArr.splice(index, 1);
+            li.remove();
+        };
+        li.appendChild(dltBtn);
+    });
+};
+addBtn.onclick = employees;
+//this expression is not constructable type 'string' has no construst signatures
--- TS-04-10/src/app.ts
+++ TS-04-10/src/app.ts
@@ -1,3 +1,11 @@
+const vardas = <HTMLInputElement> document.getElementById('vardas');
+const pavarde = <HTMLInputElement> document.getElementById('pavarde');
+const atlyginimas = <HTMLInputElement> document.getElementById('atlyginimas');
+
+const employeeList = <HTMLUListElement> document.getElementById('employeeList')!;
+
+const addBtn = document.getElementById('addBtn')!;
+
 
 
 
@@ -29,28 +37,62 @@ class Darbuotojas {
     }
 
     public gpm() {
-        return `gyventojo pajamų mokestis(GPM): ${(this.atlyginimas * 0.20).toFixed(2)}`;
+        return (this.atlyginimas * 0.20).toFixed(2);
     }
     public psd() {
-        return `privalomusis sveikatos draudimas(PSD): ${(this.atlyginimas * 0.0698).toFixed(2)}`;
+        return (this.atlyginimas * 0.0698).toFixed(2);
     }
     public vsd() {
-        return `valstybinis socialinis draudimas(VSD): ${(this.atlyginimas * 0.1252).toFixed(2)}`;
+        return (this.atlyginimas * 0.1252).toFixed(2);
     }
 }
 
 let newDarbuotojas = new Darbuotojas('Jonas','Jonaitis',1600);
-console.log(newDarbuotojas.gpm());
-console.log(newDarbuotojas.psd());
-console.log(newDarbuotojas.vsd());
+// console.log(newDarbuotojas.gpm());
+// console.log(newDarbuotojas.psd());
+// console.log(newDarbuotojas.vsd());
 
 const darbuotojuArr:Darbuotojas[] = [];
 
-darbuotojuArr.push(new Darbuotojas('Ona','Onaite',1600));
-darbuotojuArr.push(new Darbuotojas('Antantas','Antanaitis',2000));
-darbuotojuArr.push(new Darbuotojas('Petras','Petraitis',1400));
+// darbuotojuArr.push(new Darbuotojas('Ona','Onaite',1600));
+// darbuotojuArr.push(new Darbuotojas('Antantas','Antanaitis',2000));
+// darbuotojuArr.push(new Darbuotojas('Petras','Petraitis',1400));
 
-console.log(darbuotojuArr);
+
+// HTML DOM
+
+const employees = () => {
+    const getName = vardas.value;
+    const getPavarde = pavarde.value;
+    const getAtlyginimas = atlyginimas.valueAsNumber;
+
+    const naujasDarbuotojas = new Darbuotojas(getName, getPavarde, getAtlyginimas);
+
+    darbuotojuArr.push(naujasDarbuotojas);
+
+
+    employeeList.innerHTML = '';
+    darbuotojuArr.forEach((darbuotojas, index:number) => {
+        const li = document.createElement('li');
+        li.innerHTML = `${darbuotojas._vardas} ${darbuotojas._pavarde} ${darbuotojas._atlyginimas}€ <br> GPM:${darbuotojas.gpm()} PSD:${darbuotojas.psd()} VSD:${darbuotojas.vsd()}`;
+        employeeList.appendChild(li);
+
+        const dltBtn = document.createElement('button');
+        dltBtn.textContent = 'Ištrinti';
+        dltBtn.classList.add('delete-button');
+        dltBtn.onclick = () => {
+            darbuotojuArr.splice(index, 1);
+            li.remove();
+        }
+        li.appendChild(dltBtn);
+        
+    })
+    
+}
+
+addBtn.onclick = employees;
+
+//this expression is not constructable type 'string' has no construst signatures
 
 
 
